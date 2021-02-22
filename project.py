3faf8c175e3d0e83f345978a6ec681bedf77b886
khachaturyan-1991 <73434032+khachaturyan-1991@users.noreply.github.com>
2021-02-22 10:45:24 +0100

    Update project.py
    a function which doubles a number added

--- project.py
+++ project.py
@@ -6,3 +6,6 @@ print ("I expeted to a have a piece of pei this morning")
 line = "Hello world, Ruben is a bad programmer!"
 print (line)
 
+data_set = [i for i in range(3)]
+f  = lambda x: 2*x
+print(f(data_set))
